abb3664a3707cc3cbae416d24c72880dabcd0447
Jurong <1518814494@qq.com>
2019-12-25 15:45:55 +0800

    JuRong Change Tuijian

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ import Kids from '@/views/Index/Kids'
 import Men from '@/views/Index/Men'
 import Women from '@/views/Index/Women'
 import Lifestyle from '@/views/Index/Lifestyle'
-import Tuijian from '@/views/Index/tuijian'
+import Tuijian from '@/views/Index/Tuijian'
 import Qingcang from '@/views/Qingcang'
 
 Vue.use(VueRouter)
